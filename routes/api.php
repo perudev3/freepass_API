a0fbfcebb4cf0bfa7b6a4e995a6a2fb4133527ca
Stivens Espinoza Reina <stivens70espinoza@gmail.com>
2022-04-07 10:06:43 -0500

    correccion APIBeta :bug:

--- routes/api.php
+++ routes/api.php
@@ -10,49 +10,49 @@ Route::group(['prefix' => 'auth'], function () {
         
     });
 
-    Route::get('ultimos_eventos', 'ApiBeta\EventoController@lastEvents');
-    Route::get('buscarEventos', 'ApiBeta\EventoController@searchEvents');
-    Route::post('eventos_filtro', 'ApiBeta\EventoController@searchEventsTipos');
+    Route::get('ultimos_eventos', 'APIBeta\EventoController@lastEvents');
+    Route::get('buscarEventos', 'APIBeta\EventoController@searchEvents');
+    Route::post('eventos_filtro', 'APIBeta\EventoController@searchEventsTipos');
     
-    Route::get('eventos', 'ApiBeta\EventoController@index');
+    Route::get('eventos', 'APIBeta\EventoController@index');
     //listar eventos del usuario autenticado
-    Route::get('eventos-usuario','ApiBeta\EventoController@myEvents');
-    Route::get('eventos/{id}', 'ApiBeta\EventoController@show');
-    Route::post('eventos', 'ApiBeta\EventoController@store');
-    Route::put('eventos/{id}', 'ApiBeta\EventoController@update');
-    Route::delete('eventos/{id}', 'ApiBeta\EventoController@destroy');
-    Route::get('zonasevento/{evento}', 'ApiBeta\EventoController@zonasEvento');
-    Route::get('listArtistas', 'ApiBeta\EventoController@listArtistas');
-    Route::post('registrar_imagenes_evento/{evento}', 'ApiBeta\ImageController@store');
+    Route::get('eventos-usuario','APIBeta\EventoController@myEvents');
+    Route::get('eventos/{id}', 'APIBeta\EventoController@show');
+    Route::post('eventos', 'APIBeta\EventoController@store');
+    Route::put('eventos/{id}', 'APIBeta\EventoController@update');
+    Route::delete('eventos/{id}', 'APIBeta\EventoController@destroy');
+    Route::get('zonasevento/{evento}', 'APIBeta\EventoController@zonasEvento');
+    Route::get('listArtistas', 'APIBeta\EventoController@listArtistas');
+    Route::post('registrar_imagenes_evento/{evento}', 'APIBeta\ImageController@store');
 
-    Route::delete('eliminar_imagen/{imagen}', 'ApiBeta\ImageController@destroy');
+    Route::delete('eliminar_imagen/{imagen}', 'APIBeta\ImageController@destroy');
 
-    Route::get('zonas', 'ApiBeta\ZonaController@index');
-    Route::get('zonas-usuario','ApiBeta\ZonaController@myZonas');
-    Route::get('zonas/{id}', 'ApiBeta\ZonaController@show');
-    Route::post('zonas', 'ApiBeta\ZonaController@store');
-    Route::put('zonas/{id}', 'ApiBeta\ZonaController@update');
-    Route::delete('zonas/{id}', 'ApiBeta\ZonaController@destroy');
+    Route::get('zonas', 'APIBeta\ZonaController@index');
+    Route::get('zonas-usuario','APIBeta\ZonaController@myZonas');
+    Route::get('zonas/{id}', 'APIBeta\ZonaController@show');
+    Route::post('zonas', 'APIBeta\ZonaController@store');
+    Route::put('zonas/{id}', 'APIBeta\ZonaController@update');
+    Route::delete('zonas/{id}', 'APIBeta\ZonaController@destroy');
 
-    Route::get('artistas', 'ApiBeta\ArtistaController@index');
-    Route::get('artistas/{id}', 'ApiBeta\ArtistaController@show');
-    Route::post('artistas', 'ApiBeta\ArtistaController@store');
-    Route::put('artistas/{id}', 'ApiBeta\ArtistaController@update');
-    Route::delete('artistas/{id}', 'ApiBeta\ArtistaController@destroy');
+    Route::get('artistas', 'APIBeta\ArtistaController@index');
+    Route::get('artistas/{id}', 'APIBeta\ArtistaController@show');
+    Route::post('artistas', 'APIBeta\ArtistaController@store');
+    Route::put('artistas/{id}', 'APIBeta\ArtistaController@update');
+    Route::delete('artistas/{id}', 'APIBeta\ArtistaController@destroy');
     
     
-    Route::get('tipos', 'ApiBeta\TipoController@index');
-    Route::get('tipos/{id}', 'ApiBeta\TipoController@show');
-    Route::post('tipos', 'ApiBeta\TipoController@store');
-    Route::put('tipos/{id}', 'ApiBeta\TipoController@update');
-    Route::delete('tipos/{id}', 'ApiBeta\TipoController@destroy');
+    Route::get('tipos', 'APIBeta\TipoController@index');
+    Route::get('tipos/{id}', 'APIBeta\TipoController@show');
+    Route::post('tipos', 'APIBeta\TipoController@store');
+    Route::put('tipos/{id}', 'APIBeta\TipoController@update');
+    Route::delete('tipos/{id}', 'APIBeta\TipoController@destroy');
 
-    Route::get('listas', 'ApiBeta\ListaController@index');
-    Route::put('listas/{id}', 'ApiBeta\ListaController@update');
-    Route::delete('listas/{id}', 'ApiBeta\ListaController@destroy');
-    Route::post('registrar_listas/{evento}', 'ApiBeta\ListaController@store');
+    Route::get('listas', 'APIBeta\ListaController@index');
+    Route::put('listas/{id}', 'APIBeta\ListaController@update');
+    Route::delete('listas/{id}', 'APIBeta\ListaController@destroy');
+    Route::post('registrar_listas/{evento}', 'APIBeta\ListaController@store');
 
-    Route::post('registrar_invitado', 'ApiBeta\InvitadoController@store');
+    Route::post('registrar_invitado', 'APIBeta\InvitadoController@store');
 
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider');
     Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback');    
